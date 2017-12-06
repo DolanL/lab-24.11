5b199e64ed0083ffb09500873ae2ae900e00a5e2
DolanL <32385280+DolanL@users.noreply.github.com>
2017-12-06 21:42:01 +0300

    Update .cpp

--- .cpp
+++ .cpp
@@ -2,24 +2,22 @@
 #include<fstream>
 #include<iostream>
 
-int main()
-{
-    using namespace std;
-    cout << "Enter the number of students: ";
-    int n;
-    cin >> n;
-    string names;
-    string *ps = new string[n];
-    ofstream file("cppfile.txt", ios_base::out);
-    cout << "Enter names: " << endl;
-    for (int i=0; i<n; i++)
-{
-    cin >> ps[i];
-    cout << endl;
-}
-for (int i = 0; i < n; i++)
-file << ps[i] << endl;
-file.close();
-delete [] ps;
-return 0;
+int main() {
+	using namespace std;
+	cout << "Enter the number of students: ";
+	int n;
+	cin >> n;
+	string names;
+	string *ps = new string[n];
+	ofstream file("cppfile.txt", ios_base::out);
+	cout << "Enter names: " << endl;
+	for (int i=0; i<n; i++) {
+		cin >> ps[i];
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++)
+		file << ps[i] << endl;
+	file.close();
+	delete [] ps;
+	return 0;
 }
