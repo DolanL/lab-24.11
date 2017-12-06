91b67a4b6beabede15a7f0721c74b362aef69f65
DolanL <32385280+DolanL@users.noreply.github.com>
2017-12-06 21:41:18 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -2,16 +2,14 @@
 #include<fstream>
 #include<iostream>
 
-int main()
-{
-    using namespace std;
-    string buff;
-    ifstream file ("cppfile.txt");
-    for (int i=0; i < 3; i++)
-    {
-        file >> buff;
-        cout << buff;
-    }
-    file.close();
-    return 0;
+int main() {
+	using namespace std;
+	string buff;
+	ifstream file ("cppfile.txt");
+	for (int i=0; i < 3; i++) {
+		file >> buff;
+		cout << buff;
+	}
+	file.close();
+	return 0;
 }
