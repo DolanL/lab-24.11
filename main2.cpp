08ffa10a74b136eba4672fd1da07c4c0c0d4c049
DolanL <32385280+DolanL@users.noreply.github.com>
2017-12-06 21:40:21 +0300

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,17 @@
+#include<string>
+#include<fstream>
+#include<iostream>
+
+int main()
+{
+    using namespace std;
+    string buff;
+    ifstream file ("cppfile.txt");
+    for (int i=0; i < 3; i++)
+    {
+        file >> buff;
+        cout << buff;
+    }
+    file.close();
+    return 0;
+}
